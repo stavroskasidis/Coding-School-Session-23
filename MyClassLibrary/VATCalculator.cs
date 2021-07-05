c515497749fd6facbf4a58e4f017ff0b816aa6b8
stavroskasidis <stavroskasidis@outlook.com>
2021-07-05 11:22:46 +0300

    bug fix

--- MyClassLibrary/VATCalculator.cs
+++ MyClassLibrary/VATCalculator.cs
@@ -9,7 +9,7 @@ namespace MyClassLibrary
             if (netValue <= 0) throw new ArgumentException("netValue cannot be below zero", nameof(netValue));
             if (vatPercentage <= 0 || vatPercentage >= 1) throw new ArgumentException("vat percentage cannot be below zero and above 1", nameof(vatPercentage));
 
-            return netValue * (1 - vatPercentage);
+            return netValue * (1 + vatPercentage);
         }
 
         public decimal CalculateNetValue(decimal totalValue, decimal vatPercentage)
